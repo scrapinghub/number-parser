359efb85a83b37ff1695a4b043482617df9a6be0
Arnav Kapoor <arnav.kapoor@research.iiit.ac.in>
2020-07-10 12:38:25 +0530

    Updating parser to read from python language files (#23)
    * adding init file in language directory
    
    * updating code to read languge info from py files
    
    * linting pep8 updates
    
    * removing redundant paranthesis
    
    * refactoring code parser
    
    * minor code changes

--- number_parser/parser.py
+++ number_parser/parser.py
@@ -1,105 +1,93 @@
 import re
-import string
-# The initial global dictionaries that have been declared below will be changed,
-# should be easily derived automatically from CLDR data/other source for each of the languages.
+from importlib import import_module
+SENTENCE_SEPARATORS = [".", ","]
+SUPPORTED_LANGUAGES = ['en', 'es', 'hi', 'ru']
 
-# We will need to have all values from 1-100 in a dictionary for some languages like hindi while others
-# english / french build 2 digit numbers using MTEN dictionary values.
-# (Difference observable in hi.json and en.json in numeral_language_data)
 
-MULTIPLIERS = {
-    "thousand": 1000,
-    "thousands": 1000,
-    "million": 1000000,
-    "millions": 1000000,
-    "billion": 1000000000,
-    "billions": 1000000000,
-    "trillion": 1000000000000,
-    "trillions": 1000000000000,
-}
+class LanguageData:
+    """Main language class to populate the requisite language-specific variables."""
+    unit_numbers = {}
+    direct_numbers = {}
+    tens = {}
+    hundreds = {}
+    big_powers_of_ten = {}
+    skip_tokens = []
+    all_numbers = {}
+    unit_and_direct_numbers = {}
 
-VALID_TOKENS_IN_NUMBERS = ["and", "-"]
+    def __init__(self, lang_data):
+        if lang_data not in SUPPORTED_LANGUAGES:
+            raise ValueError(f'"{lang_data}" is not a supported language')
+        language_info = getattr(import_module('number_parser.data.' + lang_data), 'info')
+        self.unit_numbers = language_info["UNIT_NUMBERS"]
+        self.direct_numbers = language_info["DIRECT_NUMBERS"]
+        self.tens = language_info["TENS"]
+        self.hundreds = language_info["HUNDREDS"]
+        self.big_powers_of_ten = language_info["BIG_POWERS_OF_TEN"]
+        self.skip_tokens = language_info["SKIP_TOKENS"]
 
-UNITS = {
-    word: value
-    for value, word in enumerate(
-        "one two three four five six seven eight nine".split(), 1
-    )
-}
+        self.all_numbers = {**self.unit_numbers, **self.direct_numbers, **self.tens,
+                            **self.hundreds, **self.big_powers_of_ten}
+        self.unit_and_direct_numbers = {**self.unit_numbers, **self.direct_numbers}
 
-STENS = {
-    word: value
-    for value, word in enumerate(
-        "ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(), 10
-    )
-}
 
-MTENS = {
-    word: value * 10
-    for value, word in enumerate(
-        "twenty thirty forty fifty sixty seventy eighty ninety".split(), 2
-    )
-}
-
-HUNDRED = {"hundred": 100, "hundreds": 100}
-
-ALL_WORDS = {**UNITS, **STENS, **MTENS, **HUNDRED, **MULTIPLIERS}
-BASE_NUMBERS = {**UNITS, **STENS}
-
-def check_validity(current_token, previous_token):
+def _check_validity(current_token, previous_token, lang_data):
     """Identifies whether the new token can continue building the previous number."""
-    if (current_token in BASE_NUMBERS and previous_token in BASE_NUMBERS):
+    if current_token in lang_data.unit_and_direct_numbers and previous_token in lang_data.unit_and_direct_numbers:
         return False
 
-    elif (current_token in MTENS):
-        if (previous_token in MTENS) or (previous_token in BASE_NUMBERS):
+    if current_token in lang_data.direct_numbers and previous_token in lang_data.tens:
+        return False
+
+    elif current_token in lang_data.tens:
+        if previous_token in lang_data.tens or previous_token in lang_data.unit_and_direct_numbers:
             return False
 
-    elif (current_token in HUNDRED and previous_token in MTENS):
-        return False
+    elif current_token in lang_data.hundreds:
+        if previous_token not in lang_data.big_powers_of_ten and previous_token is not None:
+            return False
 
-    elif (current_token in MULTIPLIERS and previous_token in MULTIPLIERS):
-        return False
+    elif current_token in lang_data.big_powers_of_ten and previous_token in lang_data.big_powers_of_ten:
+        if lang_data.big_powers_of_ten[current_token] > lang_data.big_powers_of_ten[previous_token]:
+            return False
 
     return True
 
-def number_builder(token_list):
+
+def _build_number(token_list, lang_data):
     """Incrementaly builds a number from the list of tokens."""
     total_value = 0
     current_grp_value = 0
     previous_token = None
-
     value_list = []
 
     for token in token_list:
         if token.isspace():
             continue
-        valid = check_validity(token, previous_token)
+        valid = _check_validity(token, previous_token, lang_data)
         if not valid:
             total_value += current_grp_value
             value_list.append(str(total_value))
             total_value = 0
             current_grp_value = 0
-            previous_token = None
 
-        if (token in BASE_NUMBERS):
-            current_grp_value += BASE_NUMBERS[token]
+        if token in lang_data.unit_and_direct_numbers:
+            current_grp_value += lang_data.unit_and_direct_numbers[token]
 
-        elif (token in MTENS):
-            current_grp_value += MTENS[token]
+        elif token in lang_data.tens:
+            current_grp_value += lang_data.tens[token]
 
-        elif (token in HUNDRED):
-            if current_grp_value == 0:
-                current_grp_value = 1
-            current_grp_value *= 100
+        elif token in lang_data.hundreds:
+            current_grp_value += lang_data.hundreds[token]
 
-        elif (token in MULTIPLIERS):
+        elif token in lang_data.big_powers_of_ten:
             if current_grp_value == 0:
                 current_grp_value = 1
 
-            current_grp_value *= MULTIPLIERS[token]
-            total_value += current_grp_value
-            current_grp_value = 0
+            current_grp_value *= lang_data.big_powers_of_ten[token]
+            if not lang_data.big_powers_of_ten[token] == 100:
+                total_value += current_grp_value
+                current_grp_value = 0
 
         previous_token = token
 
@@ -107,38 +95,42 @@ def number_builder(token_list):
     value_list.append(str(total_value))
     return value_list
 
-SENTENCE_SEPERATORS = [".", ","]
 
-def tokenize(input_string):
+def _tokenize(input_string):
     """Breaks string on any non-word character."""
     tokens = re.split(r'(\W)', input_string)
     return tokens
 
-def parse_number(input_string):
+
+def parse_number(input_string, language='en'):
     """Converts a single number written in natural language to a numeric type"""
+    lang_data = LanguageData(language)
     if input_string.isnumeric():
         return int(input_string)
 
-    tokens = tokenize(input_string)
+    tokens = _tokenize(input_string)
+
     for index, token in enumerate(tokens):
         compare_token = token.lower()
-        if compare_token in ALL_WORDS or compare_token.isspace():
+        if compare_token in lang_data.all_numbers or compare_token.isspace() or len(compare_token) == 0:
             continue
-        if (compare_token in VALID_TOKENS_IN_NUMBERS) and (index != 0):
+        if compare_token in lang_data.skip_tokens and index != 0:
             continue
         return None
 
-    number_built = number_builder(tokens)
+    number_built = _build_number(tokens, lang_data)
     if len(number_built) == 1:
         return int(number_built[0])
     return None
 
-def parse(input_string):
+
+def parse(input_string, language='en'):
     """
     Converts all the numbers in a sentence written in natural language to their numeric type while keeping
     the other words unchanged. Returns the transformed string.
     """
-    tokens = tokenize(input_string)
+    lang_data = LanguageData(language)
+    tokens = _tokenize(input_string)
     if tokens is None:
         return None
 
@@ -148,14 +140,15 @@ def parse(input_string):
 
     for token in tokens:
         compare_token = token.lower()
-        if (compare_token.isspace() or compare_token == ""):
+
+        if compare_token.isspace() or compare_token == "":
             if not tokens_taken:
                 current_sentence.append(token)
             continue
 
-        if compare_token in SENTENCE_SEPERATORS:
+        elif compare_token in SENTENCE_SEPARATORS:
             if tokens_taken:
-                myvalue = number_builder(tokens_taken)
+                myvalue = _build_number(tokens_taken, lang_data)
                 for each_number in myvalue:
                     current_sentence.append(each_number)
                     current_sentence.append(" ")
@@ -166,12 +159,13 @@ def parse(input_string):
             current_sentence = []
             continue
 
-        if ((compare_token in ALL_WORDS) or (compare_token in VALID_TOKENS_IN_NUMBERS and len(tokens_taken) != 0)):
+        elif (compare_token in lang_data.all_numbers or
+                (compare_token in lang_data.skip_tokens and len(tokens_taken) != 0)):
             tokens_taken.append(compare_token)
 
         else:
             if tokens_taken:
-                myvalue = number_builder(tokens_taken)
+                myvalue = _build_number(tokens_taken, lang_data)
                 for each_number in myvalue:
                     current_sentence.append(each_number)
                     current_sentence.append(" ")
@@ -179,7 +173,7 @@ def parse(input_string):
             current_sentence.append(token)
 
     if tokens_taken:
-        myvalue = number_builder(tokens_taken)
+        myvalue = _build_number(tokens_taken, lang_data)
         for each_number in myvalue:
             current_sentence.append(each_number)
             current_sentence.append(" ")
