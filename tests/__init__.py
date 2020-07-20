a48c6a93369b24858e617f10a8e68ff33c0c2740
arnavkapoor <arnav.kapoor@research.iiit.ac.in>
2020-07-21 01:26:38 +0530

    adding init file for directory path

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+import os
+TEST_ROOT = os.path.dirname(os.path.abspath(__file__))
+HUNDREDS_DIRECTORY = os.path.join(TEST_ROOT, "./data/hundreds")
+PERMUTATION_DIRECTORY = os.path.join(TEST_ROOT, "./data/permutations")
