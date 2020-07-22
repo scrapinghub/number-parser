df9a54b5e2a5ebb127d75fa8d6bfa22bef62e22f
arnavkapoor <arnav.kapoor@research.iiit.ac.in>
2020-07-22 19:38:01 +0530

    reading language files function

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,3 +2,11 @@ import os
 TEST_ROOT = os.path.dirname(os.path.abspath(__file__))
 HUNDREDS_DIRECTORY = os.path.join(TEST_ROOT, "./data/hundreds")
 PERMUTATION_DIRECTORY = os.path.join(TEST_ROOT, "./data/permutations")
+
+
+def get_test_files(path, prefix):
+    return [
+        os.path.join(path, filename)
+        for filename in os.listdir(path)
+        if filename.startswith(prefix)
+    ]
