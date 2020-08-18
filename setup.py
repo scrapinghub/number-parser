03f5af1934cd2f6fb428599e4639c253618e7916
Arnav Kapoor <arnav.kapoor@research.iiit.ac.in>
2020-08-18 22:13:44 +0530

    file changes for new version release (#37)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='number-parser',
-    version='0.1.0',
+    version='0.2.0',
     description='parse numbers written in natural language',
     long_description=open('README.rst', encoding="utf8").read() + "\n\n" + open('CHANGES.rst').read(),
     author='Arnav Kapoor',
