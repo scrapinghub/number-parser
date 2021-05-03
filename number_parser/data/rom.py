c6285cb1ba6e75544a517aa62f2ae64010994d8f
AmPhIbIaN26 <43638430+AmPhIbIaN26@users.noreply.github.com>
2021-05-03 23:47:18 +0530

    Revert "Revert "Implemented parsing Roman Numerals""
    This reverts commit 81ed627b9644ff18a6271f41458d1f024b384cd0.

---
+++ number_parser/data/rom.py
@@ -0,0 +1,47 @@
+info = {
+    "UNIT_NUMBERS": {
+        "i": 1,
+        "ii": 2,
+        "iii": 3,
+        "iv": 5,
+        "vi": 6,
+        "vii": 7,
+        "viii": 8,
+        "ix": 9
+    },
+    "DIRECT_NUMBERS": {
+        "x": 10,
+
+    },
+    "TENS": {
+        "xx": 20,
+        "xxx": 30,
+        "xl": 40,
+        "l": 50,
+        "lx": 60,
+        "lxx": 70,
+        "lxxx": 80,
+        "xc": 90
+    },
+    "HUNDREDS": {
+        "c": 100,
+        "cc": 200,
+        "ccc": 300,
+        "cd": 400,
+        "d": 500,
+        "dc": 600,
+        "dcc": 700,
+        "dccc": 800,
+        "cm": 900
+    },
+    "BIG_POWERS_OF_TEN": {
+        "m": 1000,
+        "mm": 2000,
+        "mmm": 3000
+    },
+    "SKIP_TOKENS": [
+        "-",
+        "and"
+    ],
+    "USE_LONG_SCALE": False
+}
