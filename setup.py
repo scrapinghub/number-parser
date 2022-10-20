e531d3d43d7f250e6c0d0eba86a8fb111088f3c7
Serhii A <aserhii@protonmail.com>
2022-10-20 16:57:34 +0300

    Release 0.3.0 (#81)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='number-parser',
-    version='0.2.1',
+    version='0.3.0',
     description='parse numbers written in natural language',
     long_description=open('README.rst', encoding="utf8").read() + "\n\n" + open('CHANGES.rst').read(),
     author='Arnav Kapoor',
