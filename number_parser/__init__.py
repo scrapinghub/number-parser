9c96922a7528b743a77b61cbc60363a723948387
Serhii A <aserhii@protonmail.com>
2023-03-03 16:45:07 +0200

    Add __version__ (#87)
    Close #86

--- number_parser/__init__.py
+++ number_parser/__init__.py
@@ -1,1 +1,8 @@
-from number_parser.parser import parse, parse_number, parse_ordinal, parse_fraction
+import pkgutil
+
+from number_parser.parser import (parse, parse_fraction, parse_number,
+                                  parse_ordinal)
+
+__version__ = (pkgutil.get_data(__package__, "VERSION") or b"").decode("ascii").strip()
+
+del pkgutil
--- setup.py
+++ setup.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+version = (Path(__file__).parent / "number_parser/VERSION").read_text("ascii").strip()
+
 
 setup(
-    name='number-parser',
-    version='0.3.0',
-    description='parse numbers written in natural language',
-    long_description=open('README.rst', encoding="utf8").read() + "\n\n" + open('CHANGES.rst').read(),
-    author='Arnav Kapoor',
-    author_email='arnavk805@gmail.com',
-    url='https://github.com/arnavkapoor/number-parser',
-    packages=find_packages(exclude=['tests']),
+    name="number-parser",
+    version=version,
+    description="parse numbers written in natural language",
+    long_description=open("README.rst", encoding="utf8").read()
+    + "\n\n"
+    + open("CHANGES.rst").read(),
+    author="Arnav Kapoor",
+    author_email="arnavk805@gmail.com",
+    url="https://github.com/scrapinghub/number-parser/",
+    packages=find_packages(exclude=["tests"]),
     install_requires=[
-        'attrs >= 17.3.0',
+        "attrs >= 17.3.0",
     ],
     zip_safe=False,
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
